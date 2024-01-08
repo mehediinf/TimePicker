4a026c6c09f74f73664b7dc7c54928c0e26adc67
mehedi-hasan <mehediinform@gmail.com>
2024-01-09 02:04:28 +0600

    2nd Commit Time Picker

--- app/src/main/java/com/example/timepicker/MainActivity.java
+++ app/src/main/java/com/example/timepicker/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.timepicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    private Button showButton;
+    private TimePicker timePicker;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showButton = findViewById(R.id.showButton_Id);
+        timePicker = findViewById(R.id.timePicker_Id);
+        textView = findViewById(R.id.textView_Id);
+
+
+        // in this line add by set of hour 24 formate
+        timePicker.setIs24HourView(true);
+
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String time = timePicker.getCurrentHour()+":"+timePicker.getCurrentMinute();
+
+                textView.setText(time);
+
+            }
+        });
+
+
     }
 }
